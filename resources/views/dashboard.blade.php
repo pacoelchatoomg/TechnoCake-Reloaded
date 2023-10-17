86242a7bc57043a223d3c01bb1d05eb651787642
Alan Olvera <alan.olvera5111@alumnos.udg.mx>
2023-10-16 20:56:53 -0600

    validación agregada y rama actualizada

--- resources/views/dashboard.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-  
-@section('title', 'Dashboard - Laravel Admin Panel With Login and Registration')
-  
-@section('contents')
-  <div class="row">
-    Dashboard
-  </div>
-@endsection
